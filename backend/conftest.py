3277d49d51c15fdd85ddcdb1ffacc49170d86116
Michael Hays <michaelhays36@gmail.com>
2020-10-02 11:43:00 -0700

    Always allow database access when testing

--- backend/conftest.py
+++ backend/conftest.py
@@ -2,6 +2,11 @@ import pytest
 from django.core.management import call_command
 
 
+@pytest.fixture(autouse=True)
+def enable_db_access(db):
+    pass
+
+
 @pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
