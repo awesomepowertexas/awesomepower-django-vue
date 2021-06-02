1bfe3c5816cf446edbed9f22e2ef28415c32b611
Michael Hays <michaelhays36@gmail.com>
2021-06-02 10:47:09 -0700

    June 2021 TDU charges (#20)

--- backend/awesomepower/plans/tdus.py
+++ backend/awesomepower/plans/tdus.py
@@ -6,26 +6,26 @@ TDUS = [
     {
         "name": "AEP Central",
         "ptc_name": "AEP TEXAS CENTRAL COMPANY",
-        "charges": [Decimal("4.27")],
-        "rates": [Decimal("0.037458"), Decimal("0.042929")],
+        "charges": [Decimal("4.79"), Decimal("4.27")],
+        "rates": [Decimal("0.040272"), Decimal("0.037458")],
     },
     {
         "name": "AEP North",
         "ptc_name": "AEP TEXAS NORTH COMPANY",
-        "charges": [Decimal("3.57")],
-        "rates": [Decimal("0.030078"), Decimal("0.035325")],
+        "charges": [Decimal("4.79"), Decimal("3.57")],
+        "rates": [Decimal("0.036048"), Decimal("0.030078")],
     },
     {
         "name": "Centerpoint",
         "ptc_name": "CENTERPOINT ENERGY HOUSTON ELECTRIC LLC",
         "charges": [Decimal("4.39")],
-        "rates": [Decimal("0.032999"), Decimal("0.042723")],
+        "rates": [Decimal("0.033547"), Decimal("0.032999")],
     },
     {
         "name": "Oncor",
         "ptc_name": "ONCOR ELECTRIC DELIVERY COMPANY",
         "charges": [Decimal("3.42")],
-        "rates": [Decimal("0.034926"), Decimal("0.038890")],
+        "rates": [Decimal("0.034928"), Decimal("0.038890")],
     },
     {
         "name": "TNMP",
