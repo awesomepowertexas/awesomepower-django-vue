cfe87047a61533d4895312d78aa7430036aa5ffa
Michael Hays <michaelhays36@gmail.com>
2021-11-02 09:36:17 -0500

    Remove unnecessary dependencies

--- web/src/plugins/api.ts
+++ web/src/plugins/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_BASE_URL ||
+    (import.meta.env.VITE_API_BASE_URL as string) ||
     /* istanbul ignore next */ 'http://localhost:8000',
   headers: {
     Accept: 'application/json',
